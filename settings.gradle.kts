b252c328ef48f9f4736c93f8ebf470564f796f1e
roosvelt1234 <167099043+roosvelt1234@users.noreply.github.com>
2024-10-05 10:54:05 +0100

    Add files via upload

---
+++ settings.gradle.kts
@@ -0,0 +1,24 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "A.P.PSYSTEME D'AIDE A LA PRISE DE DECISION"
+include(":app")
+ 
